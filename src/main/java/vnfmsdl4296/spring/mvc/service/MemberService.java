727aa8d81e1f54b39d7274e363c1fad9b31d0aa9
pureun <vnfmsdl4296@naver.com>
2020-06-02 16:13:21 +0900

    아이디 중복체크 기능 추가

--- src/main/java/vnfmsdl4296/spring/mvc/service/MemberService.java
+++ src/main/java/vnfmsdl4296/spring/mvc/service/MemberService.java
@@ -86,4 +86,16 @@ public class MemberService {
         return json;
 
     }
+
+    // 아이디 중복체크
+    public int checkUserid(String uid) {
+        return mdao.selectUserid(uid);
+    }
+
+
+
+
+
+
+
 }
